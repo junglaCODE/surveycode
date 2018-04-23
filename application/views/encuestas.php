a1ea93718f6408b09135619e2a7a874585fb30b7
JLGC @monolinux <jlgarcia@junglacode.org>
2018-04-23 11:51:35 -0500

    add struct of materilize for style page

--- application/views/encuestas.php
+++ application/views/encuestas.php
@@ -4,15 +4,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Pruebas mother fucker</title>
+	<title>SurveyCode | Casoba</title>
+	<link rel="stylesheet" href="../__assets/materialize/css/materialize.css">
 </head>
 <body>
-
-<div id="container">
-	<?php foreach($formulario as $componente): ?>
-		<label> <?= $componente->label?> </label><br>
-		<?= $obj->switchComponents($componente->widget) ?> <br>
-	<?php endforeach;?>
-</div>
+	<div class="row">
+	<div class="col s12">
+			<?php foreach($formulario as $componente): ?>
+			<div class="row">
+				<div class="input-field col s12">
+				<label> <?= $componente->label?> </label>
+				<?= $obj->switchComponents($componente->widget) ?>
+				</div>
+			</div>		
+			<?php endforeach;?>		
+	</div>
+	</div>
+<script type="text/javascript" src="../__assets/angular/angular.min.js"></script>
+<script type="text/javascript" src="../__assets/angular/angular-route.min.js"></script>
+<script type="text/javascript" src="../__assets/materialize/js/materialize.min.js"></script>
+<script>
+ $(document).ready(function() {
+    $('select').material_select();
+  });          
+</script>
 </body>
 </html>
