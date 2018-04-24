c92b0b02e2aa20650d26b827329deab887bc6a51
JLGC @monolinux <jlgarcia@junglacode.org>
2018-04-24 17:10:48 -0500

    version functional survey 0.1.1

--- application/controllers/Encuestas.php
+++ application/controllers/Encuestas.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Encuestas extends CI_Controller {
 
-	const VERSION = '0.1.0';
+	const VERSION = '0.1.1';
 	
     public function __construct(){		
    		parent::__construct();
