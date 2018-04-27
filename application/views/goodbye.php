1aa81f2c073a249140fa364607f70c593a04abcd
JLGC @monolinux <jlgarcia@junglacode.org>
2018-04-27 12:52:42 -0500

    move workflow of me fucking work to monolinux lap

--- application/views/goodbye.php
+++ application/views/goodbye.php
@@ -1,7 +1,10 @@
+<div class="row card">
+	
+</div
 <h3 class="center-align">
   Muchas gracias. Por tu tiempo
  </h3>
-<p class="center-align">Si deseas información acerca de nuestros calentadores solares
+<p class="center-align">Quieres que te proporcioné información acerca de nuestros calentadores solares.
 	<b>Ingresa tu email y enviaremos nuestros catálogos sin ningún compromiso.</b>
 </p>
 <div class="row">
@@ -15,5 +18,8 @@
 		<button type="submit" class="waves-effect waves-light btn-large col s12">
 			Listo. He finalizado
 		</button>
+	</div>
+	<div class="progress">
+      <div class="indeterminate"></div>
 	</div>
 </div>
