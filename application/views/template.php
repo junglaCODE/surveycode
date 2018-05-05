c5b0754bbcce7fa7a2966f3af216f1c1e0b3a29b
JLGC @monolinux <jlgarcia@junglacode.org>
2018-05-05 10:11:47 -0500

    agregando nuevos componentes al template

--- application/views/template.php
+++ application/views/template.php
@@ -11,8 +11,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <link rel="stylesheet" href="../__assets/dist/css/_style.css">
 </head>
 <body ng-controller="_formSurvey_ as survey">
+<nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">SurveyCode</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href=".">Encuesta</a></li>
+        <li><a href="#!monitor">Monitoreo</a></li>
+      </ul>
+    </div>
+  </nav>
 	<div class="row">
-		<div class="col s12 m8 offset-m2  card">
+		<div class="col s12 m8 offset-m2 card">
 			<form ng-submit="survey.finished(survey.form)">
 				<div ng-view></div>
 			</form>
@@ -22,6 +31,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <script type="text/javascript" src="../__assets/angular/angular.min.js"></script>
 <script type="text/javascript" src="../__assets/angular/angular-route.min.js"></script>
 <script type="text/javascript" src="../__assets/materialize/js/materialize.min.js"></script>
+<script type="text/javascript" src="../__assets/materialize/js/materialize.min.js"></script>
+<script type="text/javascript" src="../__assets/angular-chart/Chart.min.js"></script>
+<script type="text/javascript" src="../__assets/angular-chart/angular-chart.min.js"></script>
 <script type="text/javascript" src="../__assets/thirdparty/checklist-model.js"></script>
 <script type="text/javascript" src="../__assets/dist/js/formsurvey.js"></script>
 </body>
